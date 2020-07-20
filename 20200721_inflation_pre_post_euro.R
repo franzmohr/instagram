c62bcbd0d782a7944f2fb7098609e4fa31caf1b4
acer-fxm <franz.x.mohr@outlook.com>
2020-07-21 00:45:28 +0200

    Inflation pre and post euro introduction

---
+++ 20200721_inflation_pre_post_euro.R
@@ -0,0 +1,63 @@
+ rm(list = ls())
+
+library(dplyr)
+library(ggplot2)
+library(OECD) # install.packages("OECD")
+library(tidyr)
+library(zoo)
+
+#### Inflation ####
+p <- get_dataset("PRICES_CPI", filter = "AUT.CPALTT01.IXOB.Q") %>%
+  select(obsTime, obsValue) %>%
+  rename(value = obsValue) %>%
+  mutate(date = as.yearqtr(obsTime, "%Y-Q%q"),
+         growth = value / lag(value, 4) - 1) %>%
+  filter(!is.na(growth))
+
+t_diff <- 20 * 4
+
+pre <- p %>%
+  filter(date < "1999 Q1") %>%
+  tail(t_diff) %>%
+  mutate(type = "pre")
+
+post <- p %>%
+  filter(date >= "1999 Q1") %>%
+  slice(1:t_diff) %>%
+  mutate(type = "post")
+
+temp <- bind_rows(pre, post) %>%
+  group_by(type) %>%
+  mutate(time = 1:n() / 4,
+         index = value / value[1] * 100) %>%
+  ungroup() %>%
+  mutate(type_de = factor(type, levels = c("pre", "post"),
+                          labels = c("20 Jahre vor der Einführung des Euro\n(1979Q1 und 1998Q4)",
+                                     "20 Jahre ab der Einführung des Euro\n(1999Q1 und 2018Q4)")))
+
+source("theme_instagram.R")
+
+g <- ggplot(temp, aes(x = time, y = index, colour = type_de)) +
+  geom_line(size = 1.2) +
+  scale_x_continuous(expand = c(.01, .1)) +
+  labs(title = "Inflation vor und nach Einführung des Euro (Österreich)",
+       subtitle = "Index, 1979Q1 = 100 (rot) bzw. 1999Q1 = 100 (blau)",
+       caption = "Quelle: OECD. Code unter https://github.com/franzmohr/instagram.") +
+  scale_colour_insta +
+  theme_instagram +
+  theme(axis.text.x = element_blank())
+
+ggsave(g, filename = "pics/20200721_inflation_pre_post_eu_de.jpeg", height = 5, width = 5)
+
+
+
+g <- ggplot(temp, aes(x = date, y = value, colour = name_en)) +
+  geom_line(size = 1.2) +
+  scale_x_yearqtr(expand = c(.01, 0), format = "%YQ%q", n = 10) +
+  labs(title = "Real gross domestic product (Austria)",
+       subtitle = "Bn EUR",
+       caption = "Source: OECD. Quarterly data (2010 prices). Hodrick-Prescott-filter (\u03BB = 1600).") +
+  scale_colour_insta +
+  theme_instagram
+
+ggsave(g, filename = "pics/20200717_trend_cycle_en.jpeg", height = 5, width = 5)
